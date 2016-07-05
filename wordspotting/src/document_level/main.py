cbb5f1796a9316c7ef6c22e36951324208fdee41
da1605 <da1605@patrec-debian.cs.tu-dortmund.de>
2016-07-05 17:51:02 +0200

    Main hinzugefügt und teilweise implementiert

--- wordspotting/src/document_level/main.py
+++ wordspotting/src/document_level/main.py
@@ -15,92 +15,104 @@ from matplotlib.patches import Circle, Rectangle
 from matplotlib.lines import Line2D
 from scipy import signal
 from scipy import misc
+import scipy.spatial
 #from visualization import hbar_plot, bar_plot
 from collections import defaultdict
 
+class SiftCalculator(object):
+    def __init__(self, step_size, cell_size, n_centroids):
+        self.step_size = step_size
+        self.cell_size = cell_size
+        self.n_centroids = n_centroids
+        self.centroids = None
 
-def visual_word_description(document_image_filename, step_size, cell_size, n_centroids, visualize = True):
-
-   
-    image = Image.open(document_image_filename)
-    # Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen. 
-    im_arr = np.asarray(image, dtype='float32')
-    # Die colormap legt fest wie die Intensitaetswerte interpretiert werden.
-    if visualize:
-        plt.imshow(im_arr, cmap=cm.get_cmap('Greys_r'))
-        plt.show()
-    
-    
-    
-    # SIFT Deskriptoren berechnen
-    frames, desc = vlfeat.vl_dsift(im_arr, step=step_size, size=cell_size)
-#     pickle_densesift_fn = '2700270-small_dense-%d_sift-%d_descriptors.p' % (step_size, cell_size)
-#     frames, desc = pickle.load(open(pickle_densesift_fn, 'rb'))
-    frames = frames.T
-    desc = desc.T
-
-    # 
-    # Um eine Bag-of-Features Repraesentation des Bilds zu erstellen, wird ein
-    # Visual Vocabulary benoetigt. Im Folgenden wird es in einer Clusteranalyse
-    # berechnet. Fuer die Clusteranalyse wird Lloyd's Version des k-means Algorithmus
-    # verwendet. Parameter sind
-    # - die Anzahl der Centroiden in der Clusteranalyse (n_centroids). Das entspricht 
-    # der Groesse des Visual Vocabulary bzw. der Anzahl von Visual Words. 
-    # - Der Anzahl von Durchlaeufen des Algorithmus (iter)
-    # - Der Initialisierung (minit). Der Wert 'points' fuehrt zu einer zufaelligen
-    #   Auswahl von gegebenen Datenpunkten, die als initiale Centroiden verwendet
-    #   werden.
-    # Die Methode gibt zwei NumPy Arrays zurueck: 
-    #  - Das sogenannte Codebuch. Eine zeilenweise organisierte Matrix mit Centroiden (hier nicht verwendet).
-    #  - Einen Vektor mit einem Index fuer jeden Deskriptor in desc. Der Index bezieht
-    #    sich auf den aehnlichsten Centroiden aus dem Codebuch (labels).
-    #
-    # Die Abbildung von Deskriptoren auf Centroiden (Visual Words) bezeichnet man als Quantisierung.
-    
-    centroids, labels = kmeans2(desc, n_centroids, iter=20, minit='points')
-    #
-    # Die Deskriptoren und deren Quantisierung werden nun visualisiert. Zu jedem 
-    # Deskriptor werden dazu die Mittelpunkte und die 4x4 Zellen eingezeichnet.
-    # Die Farbe des Mittelpunkts codiert den Index des Visual Words im Visual Vocabulary
-    # (Codebuch). Beachten Sie, dass durch diese Kodierung einige Farben sehr 
-    # aehnlich sein koennen. 
-    # Da das Zeichnen der 4x4 Zellen fuer jeden Deskriptor viel Performance kosten
-    # kann, ist es moeglich es ueber das Flag draw_descriptor_cells abzuschalten.
-    #
-    if visualize:
-        draw_descriptor_cells = True
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.imshow(im_arr, cmap=cm.get_cmap('Greys_r'))
-        ax.hold(True)
-        ax.autoscale(enable=False)
-        colormap = cm.get_cmap('jet')
-        desc_len = cell_size * 4
-        for (x, y), label in zip(frames, labels):
-            color = colormap(label / float(n_centroids))
-            circle = Circle((x, y), radius=1, fc=color, ec=color, alpha=1)
-            rect = Rectangle((x - desc_len / 2, y - desc_len / 2), desc_len, desc_len, alpha=0.08, lw=1)
-            ax.add_patch(circle)
-            if draw_descriptor_cells:
-                for p_factor in [0.25, 0.5, 0.75]:
-                    offset_dyn = desc_len * (0.5 - p_factor)
-                    offset_stat = desc_len * 0.5
-                    line_h = Line2D((x - offset_stat, x + offset_stat), (y - offset_dyn, y - offset_dyn), alpha=0.08, lw=1)
-                    line_v = Line2D((x - offset_dyn , x - offset_dyn), (y - offset_stat, y + offset_stat), alpha=0.08, lw=1)
-                    ax.add_line(line_h)
-                    ax.add_line(line_v)
-            ax.add_patch(rect)
+    def calculate_visual_words_for_document(self, document_image_filename, visualize = False):
+        image = Image.open(document_image_filename)
+        # Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen. 
+        im_arr = np.asarray(image, dtype='float32')
+        # Die colormap legt fest wie die Intensitaetswerte interpretiert werden.
+        if visualize:
+            plt.imshow(im_arr, cmap=cm.get_cmap('Greys_r'))
+            plt.show()
         
-        plt.show()
+        
+        
+        # SIFT Deskriptoren berechnen
+        frames, desc = vlfeat.vl_dsift(im_arr, step=self.step_size, size=self.cell_size)
+    #     pickle_densesift_fn = '2700270-small_dense-%d_sift-%d_descriptors.p' % (step_size, cell_size)
+    #     frames, desc = pickle.load(open(pickle_densesift_fn, 'rb'))
+        frames = frames.T
+        desc = desc.T
     
-    # Centroids: Eine Liste von Zentroiden (Auch SWIFT Operatoren!)
-    # Labels: Fuer jeden SWIFT Operator ist ein Index vorhanden, der angibt, zu welchem Zentroid der Operator zugeordnet ist.
-    # ORDNUNG: Spaltenweise von oben nach unten und links nach rechts. Beispiel:
-    # 1 4 7
-    # 2 5 8
-    # 3 6 9
-    return centroids, labels
+        # 
+        # Um eine Bag-of-Features Repraesentation des Bilds zu erstellen, wird ein
+        # Visual Vocabulary benoetigt. Im Folgenden wird es in einer Clusteranalyse
+        # berechnet. Fuer die Clusteranalyse wird Lloyd's Version des k-means Algorithmus
+        # verwendet. Parameter sind
+        # - die Anzahl der Centroiden in der Clusteranalyse (n_centroids). Das entspricht 
+        # der Groesse des Visual Vocabulary bzw. der Anzahl von Visual Words. 
+        # - Der Anzahl von Durchlaeufen des Algorithmus (iter)
+        # - Der Initialisierung (minit). Der Wert 'points' fuehrt zu einer zufaelligen
+        #   Auswahl von gegebenen Datenpunkten, die als initiale Centroiden verwendet
+        #   werden.
+        # Die Methode gibt zwei NumPy Arrays zurueck: 
+        #  - Das sogenannte Codebuch. Eine zeilenweise organisierte Matrix mit Centroiden (hier nicht verwendet).
+        #  - Einen Vektor mit einem Index fuer jeden Deskriptor in desc. Der Index bezieht
+        #    sich auf den aehnlichsten Centroiden aus dem Codebuch (labels).
+        #
+        # Die Abbildung von Deskriptoren auf Centroiden (Visual Words) bezeichnet man als Quantisierung.
+        
+        centroids, labels = kmeans2(desc, self.n_centroids, iter=20, minit='points')
+        #
+        # Die Deskriptoren und deren Quantisierung werden nun visualisiert. Zu jedem 
+        # Deskriptor werden dazu die Mittelpunkte und die 4x4 Zellen eingezeichnet.
+        # Die Farbe des Mittelpunkts codiert den Index des Visual Words im Visual Vocabulary
+        # (Codebuch). Beachten Sie, dass durch diese Kodierung einige Farben sehr 
+        # aehnlich sein koennen. 
+        # Da das Zeichnen der 4x4 Zellen fuer jeden Deskriptor viel Performance kosten
+        # kann, ist es moeglich es ueber das Flag draw_descriptor_cells abzuschalten.
+        #
+        if visualize:
+            draw_descriptor_cells = True
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.imshow(im_arr, cmap=cm.get_cmap('Greys_r'))
+            ax.hold(True)
+            ax.autoscale(enable=False)
+            colormap = cm.get_cmap('jet')
+            desc_len = self.cell_size * 4
+            for (x, y), label in zip(frames, labels):
+                color = colormap(label / float(self.n_centroids))
+                circle = Circle((x, y), radius=1, fc=color, ec=color, alpha=1)
+                rect = Rectangle((x - desc_len / 2, y - desc_len / 2), desc_len, desc_len, alpha=0.08, lw=1)
+                ax.add_patch(circle)
+                if draw_descriptor_cells:
+                    for p_factor in [0.25, 0.5, 0.75]:
+                        offset_dyn = desc_len * (0.5 - p_factor)
+                        offset_stat = desc_len * 0.5
+                        line_h = Line2D((x - offset_stat, x + offset_stat), (y - offset_dyn, y - offset_dyn), alpha=0.08, lw=1)
+                        line_v = Line2D((x - offset_dyn , x - offset_dyn), (y - offset_stat, y + offset_stat), alpha=0.08, lw=1)
+                        ax.add_line(line_h)
+                        ax.add_line(line_v)
+                ax.add_patch(rect)
+            
+            
+            
+            plt.show()
+        
+        # Centroids: Eine Liste von Zentroiden (Auch SWIFT Operatoren!)
+        # Labels: Fuer jeden SWIFT Operator ist ein Index vorhanden, der angibt, zu welchem Zentroid der Operator zugeordnet ist.
+        # ORDNUNG: Spaltenweise von oben nach unten und links nach rechts. Beispiel:
+        # 1 4 7
+        # 2 5 8
+        # 3 6 9
+        self.centroids = centroids
+        labels = np.reshape(labels,(1+int((im_arr.shape[0]-(1.5*self.cell_size))/self.step_size),-1),order='F')
+
+        
+        return centroids, labels
 
 
 if __name__ == '__main__':
-    visual_word_description('../../george_washington_files/2710271.png', 150, 15, 5);
+    siftcalc = SiftCalculator( 150, 15, 5)
+    siftcalc.calculate_visual_words_for_document('../../george_washington_files/2710271.png', visualize = True)
---
+++ wordspotting/src/main/main_sequence.py
@@ -0,0 +1,45 @@
+import document_level.main
+import numpy as np
+import Image
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+sift_step_size = 150
+sift_cell_size = 15
+sift_n_classes = 5
+
+patch_width = 300
+patch_height = 75
+patch_hop_size = 25 
+
+searchfile = '../../george_washington_files/2700270.png'
+image = Image.open(searchfile)
+# Fuer spaeter folgende Verarbeitungsschritte muss das Bild mit float32-Werten vorliegen. 
+im_arr = np.asarray(image, dtype='float32')
+dimensions = im_arr.shape
+print dimensions
+# 1043 671 1443 765 companies
+groundtrouth = (1043, 671, 1443, 765, "companies")
+distance_to_end_x = dimensions[0]- groundtrouth[2]
+distance_to_end_y = dimensions[1]- groundtrouth[3]
+query = (groundtrouth[0] - (patch_width - min(distance_to_end_x, patch_width)),
+         groundtrouth[1] - (patch_height - min(distance_to_end_y, patch_height)), 
+         groundtrouth[0] + min(distance_to_end_x, patch_width),
+         groundtrouth[1] + min(distance_to_end_y, patch_height)
+         )
+
+query_im = im_arr[query[1]:query[3], query[0]:query[2]]
+plt.imshow(query_im, cmap=cm.get_cmap('Greys_r'))
+plt.show()
+
+siftcalc = document_level.main.SiftCalculator(sift_step_size, sift_cell_size, sift_n_classes)
+centroids, labels = siftcalc.calculate_visual_words_for_document(searchfile, visualize = True)
+
+
+print labels
+
+
+
+
+if __name__ == '__main__':
+    pass
