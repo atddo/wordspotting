cce53a7df9b01635e9343f35bd1a2b0fb3864e7d
smsnbrod <steven.brodziak@tu-dortmund.de>
2016-07-14 18:55:55 +0200

    overlap berechnung implementiert ebenfalls nicht getestet

--- wordspotting/src/patch_level/main.py
+++ wordspotting/src/patch_level/main.py
@@ -76,3 +76,45 @@ class feature_vector_descriptor(object):
                 return True
         else:
             return False
+
+    def calculate_overlap(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        if x1 <= x3 & x2 > x3:
+            if x2 <= x4:
+                x = x2 - x3
+            else:
+                x = x4 - x3
+            if y1 <= y3 & y2 > y3:
+                if y2 <= y4:
+                    y = y2 - y3
+                else:
+                    y = y4 - y3
+            elif y1 > y3 & y1 <= y4:
+                if y2 <= y4:
+                    y = y2 - y1
+                else:
+                    y = y4 - y1
+        elif x1 > x3 & x1 <= x4:
+            if x2 <= x4:
+                x = x2 - x1
+            else:
+                x = x4 - x1
+            if y1 <= y3 & y2 > y3:
+                if y2 <= y4:
+                    y = y2 - y3
+                else:
+                    y = y4 - y3
+            elif y1 > y3 & y1 <= y4:
+                if y2 <= y4:
+                    y = y2 - y1
+                else:
+                    y = y4 - y1
+        section = x * y
+
+        r1_area = (x2 - x1) * (y2 - y1)
+        r2_area = (x4 - x3) * (y4 - y3)
+
+        union = r1_area + r2_area - section
+
+        overlap = section / union
+
+        return overlap
