ebaca85e3c0986de8df607d25b862c880f31e7e0
da1605 <da1605@patrec-debian.cs.tu-dortmund.de>
2016-07-15 13:02:14 +0200

    so Sachen

--- wordspotting/src/main/main_sequence.py
+++ wordspotting/src/main/main_sequence.py
@@ -7,6 +7,9 @@ def roundTo(n,base):
         n+=1
     return n
 
+def eval(truth_list, result_list):
+    return (1,1,1)
+
 # patch_hop_size = n*sift_step_size
 # patch_width = m*sift_step_size
 # patch_height = i*sift_step_size
@@ -15,7 +18,6 @@ sift_step_size = 5
 sift_cell_size = 15
 sift_n_classes = 2000
 
-patch_width = 300
 patch_height = 75
 patch_hop_size = 15
 metric = 'cosine'
@@ -25,18 +27,11 @@ flatten_dimensions = 1000
 visualize_progress=False
 searchfile = '../../george_washington_files/2700270.png'
 
-
-#my_finder = word_finder(sift_step_size, sift_cell_size, sift_n_classes, patch_width, patch_height, patch_hop_size, flatten_dimensions, searchfile, visualize_progress)
-#my_finder.search((580, 319, 723, 406, "the"))
-#my_finder.search((1235, 1518, 1450, 1622, "with"))
-
-
 page = searchfile.split('/')[-1].split('.')[0]
 gt_file = '../../groundtruth/'+page+'.gtp'
 
 with open(gt_file) as f:
     gts = f.readlines()
-
 
 positions = collections.defaultdict(lambda: [])
 for line in gts:
@@ -47,16 +42,17 @@ for line in gts:
     positions[word].append((int(line[0]),int(line[1]),int(line[2]),int(line[3])))
 
 
-
+eval_list = []
 for word in positions.keys():
     print word
     if len( positions[word])>1:
         for position in positions[word]:
             query_width = position[2] - position[0]
             width = roundTo(query_width, 50)
-            print width
+            
             my_finder = Word_finder(sift_step_size, sift_cell_size, sift_n_classes, width, patch_height, patch_hop_size, flatten_dimensions, searchfile, visualize_progress)
             result = my_finder.search(position)
+            eval_list.append(eval(positions[word],result))
              
 
 
